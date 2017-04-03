36f0ec579a795ab9fd19bce71cea3f65910f7d86
Sara Gallagher <sara.p.gallagher@gmail.com>
2017-04-03 13:40:43 -0700

    updated pictures and added randomization to which player goes first

--- apps.js
+++ apps.js
@@ -7,25 +7,26 @@ var playerTwo = {
     left3:'<img src="img/bleft-three.png" >',
     left4:'<img src="img/bleft-four.png" >',
     left5:'<img src="img/bleft-five.png" >'
-  },
-  bottomRight:{
-    right1:'<img src="img/bright-one.png" >',
-    right2:'<img src="img/bright-two.png" >',
-    right3:'<img src="img/bright-three.png" >',
-    right4:'<img src="img/bbright-four.png" >',
-    right5:'<img src="img/bright-five.png" >'
   }
+  // },
+  // bottomRight:{
+  //   right1:'<img src="img/bright-one.png" >',
+  //   right2:'<img src="img/bright-two.png" >',
+  //   right3:'<img src="img/bright-three.png" >',
+  //   right4:'<img src="img/bbright-four.png" >',
+  //   right5:'<img src="img/bright-five.png" >'
+  // }
 }
 var playerOne = {
   name: 'Player One',
   backgroundColor: 'red',
-  topRight:{
-    right1:'<img src="img/tright-one.png" >',
-    right2:'<img src="img/tright-two.png" >',
-    right3:'<img src="img/tright-three.png" >',
-    right4:'<img src="img/tbright-four.png" >',
-    right5:'<img src="img/tright-five.png" >'
-  },
+  // topRight:{
+  //   right1:'<img src="img/tright-one.png" >',
+  //   right2:'<img src="img/tright-two.png" >',
+  //   right3:'<img src="img/tright-three.png" >',
+  //   right4:'<img src="img/tbright-four.png" >',
+  //   right5:'<img src="img/tright-five.png" >'
+  // },
   topLeft: {
     left1:'<img src="img/tleft-one.png" >',
     left2:'<img src="img/tleft-two.png" >',
@@ -39,31 +40,35 @@ var gameBoard = $('.game-board')
 var timer = $('#timer')
 var count = 20;
 var bottomLeft = $('#bottom-left')
-var bottomRight = $('#bottom-right')
-var topRight = $('#top-right')
+// var bottomRight = $('#bottom-right')
+// var topRight = $('#top-right')
 var topLeft = $('#top-left')
+var players = [
+  playerOne, playerTwo
+]
 
-var currentPlayer = playerOne.name
+var currentPlayer = players[Math.round(Math.random())].name
+
+//use Math.round(Math.random()) to determine a random first player
 
 gameBoard.on('click', playerTurn)
 
 function playerTurn(){
-  // console.log('turn')
-  $('#turn').text("It's " + currentPlayer.name + "'s Turn!")
+  $('#turn').text(currentPlayer + " gets to start!")
 
-  switchTurns()
+  //switchTurns()
 }
 
-function switchTurns(){
-  if (currentPlayer == playerOne){
-    currentPlayer = playerTwo
-    bottomLeft.css('backgroundColor', 'blue')
-  } else{
-    currentPlayer = playerOne
-    topLeft.css('backgroundColor', 'red')
-
-  }
-}
+// function switchTurns(){
+//   if (currentPlayer == playerOne){
+//     currentPlayer = playerTwo
+//     bottomLeft.css('backgroundColor', 'blue')
+//   } else{
+//     currentPlayer = playerOne
+//     topLeft.css('backgroundColor', 'red')
+//
+//   }
+// }
 
 function countDown(){
   timer.text('Time Left :' + count)
@@ -80,43 +85,26 @@ $('button').on('click',function(){
 })
 
 bottomLeft.on('click', increaseBLHands)
-// bottomRight.on('click',increaseBRHands)
 topLeft.on('click', increaseTLHands)
-// topRight.on('click', increaseTRHands)
-
 
 function increaseBLHands(){
 
+  if(topLeft.html(playerOne.topLeft.left3)){
+    bottomLeft.on('click', function(){
+      bottomLeft.html(playerTwo.bottomLeft.left5)
+    })
+  }
+}
+
+function increaseTLHands(){
   if(topLeft.html(playerOne.topLeft.left1)){
-    bottomLeft.on('click',function(){
+    bottomLeft.on('click', function(){
        bottomLeft.html(playerTwo.bottomLeft.left2)
      })
   }
-  // if(topLeft.html(playerOne.topLeft.left)){
-  //   bottomLeft.html(playerTwo.bottomLeft.left5)
-  //
-  // }
-}
-
-
-
-function increaseTLHands(){
-  if (bottomLeft.html(playerTwo.bottomLeft.left1)){
+  if (bottomLeft.html(playerTwo.bottomLeft.left2)){
     topLeft.on('click', function(){
-      topLeft.html(playerOne.topLeft.leftTwo)
+      topLeft.html(playerOne.topLeft.left3)
     })
   }
-  // if (bottomLeft.html(playerTwo.bottomLeft.left2)){
-  //   topLeft.on('click', function(){
-  //     topLeft.html(playerOne.topLeft.leftThre)
-  //   })
-  // }
 }
-
-
-// function increaseBRHands(){
-//     bottomRight.html(playerTwo.bottomRight.rightTwo)
-// }
-// function increaseTRHands(){
-//   topRight.html(playerOne.topRight.rightTwo)
-// }
