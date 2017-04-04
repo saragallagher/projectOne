91445829ee1194b86f2c6f5d21bbf1b82df5beec
Sara Gallagher <sara.p.gallagher@gmail.com>
2017-04-04 10:33:44 -0700

    got the distribute function to work for one set

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ var playerOne = {
   rightFive: '<img src="img/tright-five.png">'
 
 }
-
 var playerTwo = {
   selectLeft: document.querySelector('#bottom-left'),
   selectRight: document.querySelector('#bottom-right'),
@@ -54,6 +53,7 @@ start.addEventListener('click', function(){
   whichTurn()
   playGame()
 })
+distribute.addEventListener('click',distributeFingers)
 
 function whichTurn(){
   turn.innerHTML = currentPlayer.name + " get's to start!"
@@ -70,8 +70,10 @@ function countDown(){
 function playGame(){
   //when the top player starts
   if(currentPlayer == players[0]){
-    playerTwo.selectLeft.addEventListener('click', playBottomLeft)
-    playerOne.selectLeft.addEventListener('click', playTopLeft)
+    playerTwo.selectLeft.addEventListener('click', player1BottomLeft)
+    playerOne.selectLeft.addEventListener('click', player1TopLeft)
+    playerTwo.selectRight.addEventListener('click',player1BottomRight)
+    playerOne.selectRight.addEventListener('click',player1TopRight)
   }
   //when the bottom player starts
   else if (currentPlayer == player[1]){
@@ -79,18 +81,62 @@ function playGame(){
   }
 }
 
-function playBottomLeft(){
+function player1BottomLeft(){
   if(playerOne.selectLeft.innerHTML == '<img src="img/tleft-one.png">'){
     playerTwo.selectLeft.innerHTML = playerTwo.leftTwo
   }
-  if(playerOne.selectLeft.innerHTML == playerOne.leftThree){
+  if(playerOne.selectLeft.innerHTML == playerOne.leftTwo){
+    playerTwo.selectLeft.innerHTML = playerTwo.leftThree
+  }
+  if(playerOne.selectLeft.innerHTML == playerOne.leftThree && playerTwo.selectLeft.innerHTML == '<img src="img/bleft-one.png">'){
+    playerTwo.selectLeft.innerHTML = playerTwo.leftFour
+  }
+  if(playerOne.selectLeft.innerHTML == playerOne.leftThree && playerTwo.selectLeft.innerHTML == playerTwo.leftTwo){
     playerTwo.selectLeft.innerHTML = playerTwo.leftFive
-    console.log('You Lost :(')  
+    console.log('You Lost :(')
   }
 }
 
-function playTopLeft(){
-  if(playerTwo.selectLeft.innerHTML == playerTwo.leftTwo){
+function player1TopLeft(){
+  if(playerTwo.selectLeft.innerHTML == '<img src="img/bleft-one.png">' || playerTwo.selectLeft.innerHTML == playerTwo.leftTwo){
     playerOne.selectLeft.innerHTML = playerOne.leftThree
   }
+  if(playerTwo.selectLeft.innerHTML==playerTwo.leftThree){
+    playerOne.selectLeft.innerHTML = playerOne.leftFive
+    console.log('You Lost :(')
+  }
+  // if(playerTwo.selectLeft.innerHTML == playerTwo.leftTwo && playerOne.selectLeft.innerHTML == playerOne.leftTwo){
+  //   playerOne.selectLeft.innerHTML = playerOne.leftFour
+  // }
+}
+
+function player1BottomRight(){
+  if(playerOne.selectRight.innerHTML == '<img src="img/tright-one.png">'){
+    playerTwo.selectRight.innerHTML = playerTwo.rightTwo
+  }
+  if(playerOne.selectRight.innerHTML == playerOne.rightTwo && playerTwo.selectRight.innerHTML == playerTwo.rightTwo){
+    playerTwo.selectRight.innerHTML = playerTwo.rightFour
+  }
+  if(playerOne.selectRight.innerHTML == playerOne.rightThree){
+    playerTwo.selectRight.innerHTML = playerTwo.rightFive
+    console.log('You Lost :(')
+  }
+}
+
+function player1TopRight(){
+  if(playerTwo.selectRight.innerHTML == playerTwo.rightTwo && playerOne.selectRight.innerHTML == '<img src="img/tright-one.png">'){
+    playerOne.selectRight.innerHTML = playerOne.rightThree
+  }
+  if(playerTwo.selectRight.innerHTML == playerTwo.rightTwo && playerOne.selectRight.innerHTML == playerOne.rightTwo){
+    playerOne.selectRight.innerHTML = playerOne.rightFour
+  }
+}
+
+
+function distributeFingers(){
+  if(playerOne.selectRight.innerHTML == playerOne.rightThree && playerOne.selectLeft.innerHTML == '<img src="img/tleft-one.png">'){
+    playerOne.selectRight.innerHTML = playerOne.rightTwo
+    playerOne.selectLeft.innerHTML = playerOne.leftTwo
+  }
+
 }
