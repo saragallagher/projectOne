ca307c1a8c62e6f56219ccfde3e65e38712c6b4d
Sara Gallagher <sara.p.gallagher@gmail.com>
2017-04-04 10:56:34 -0700

    added score for one outcome

--- main.js
+++ main.js
@@ -42,8 +42,10 @@ var timer = document.querySelector('#timer')
 var turn = document.querySelector('#turn')
 var count = 20;
 var players = [topPlayer, bottomPlayer]
-
+var scoreP1 = document.querySelector('#score-p1')
+var scoreP2 = document.querySelector('#score-p2')
 var currentPlayer = players[0]
+
 
 reload.addEventListener('click', function(){
   document.location.reload(true)
@@ -52,8 +54,11 @@ start.addEventListener('click', function(){
   theInterval = setInterval(countDown, 1000)
   whichTurn()
   playGame()
+
+
 })
 distribute.addEventListener('click',distributeFingers)
+
 
 function whichTurn(){
   turn.innerHTML = currentPlayer.name + " get's to start!"
@@ -70,6 +75,7 @@ function countDown(){
 function playGame(){
   //when the top player starts
   if(currentPlayer == players[0]){
+
     playerTwo.selectLeft.addEventListener('click', player1BottomLeft)
     playerOne.selectLeft.addEventListener('click', player1TopLeft)
     playerTwo.selectRight.addEventListener('click',player1BottomRight)
@@ -94,6 +100,7 @@ function player1BottomLeft(){
   if(playerOne.selectLeft.innerHTML == playerOne.leftThree && playerTwo.selectLeft.innerHTML == playerTwo.leftTwo){
     playerTwo.selectLeft.innerHTML = playerTwo.leftFive
     console.log('You Lost :(')
+    scoreP1.innerText ='Player One: 1'
   }
 }
 
@@ -104,6 +111,7 @@ function player1TopLeft(){
   if(playerTwo.selectLeft.innerHTML==playerTwo.leftThree){
     playerOne.selectLeft.innerHTML = playerOne.leftFive
     console.log('You Lost :(')
+    scoreP2.innerText = 'Player Two: 1'
   }
   // if(playerTwo.selectLeft.innerHTML == playerTwo.leftTwo && playerOne.selectLeft.innerHTML == playerOne.leftTwo){
   //   playerOne.selectLeft.innerHTML = playerOne.leftFour
@@ -120,6 +128,7 @@ function player1BottomRight(){
   if(playerOne.selectRight.innerHTML == playerOne.rightThree){
     playerTwo.selectRight.innerHTML = playerTwo.rightFive
     console.log('You Lost :(')
+    scoreP1.innerText ='Player One: 1'
   }
 }
 
@@ -131,7 +140,6 @@ function player1TopRight(){
     playerOne.selectRight.innerHTML = playerOne.rightFour
   }
 }
-
 
 function distributeFingers(){
   if(playerOne.selectRight.innerHTML == playerOne.rightThree && playerOne.selectLeft.innerHTML == '<img src="img/tleft-one.png">'){
