a127f5f3ed34f15e15ea9cd9ce5e9de8ed52d84a
Sara Gallagher <sara.p.gallagher@gmail.com>
2017-04-06 15:03:02 -0700

    added countdown

---
+++ new.js
@@ -0,0 +1,34 @@
+var $finger = $('.finger')
+var $start = $('#start')
+var count = 10;
+var $timer = $('#timer')
+var $gameBoard = $('.game-board')
+
+$finger.on('click', increase)
+
+function increase(){
+  var hand = $(this).attr("class").split(' ')[2]
+    console.log(hand);
+  var total = 0
+    $('.' + hand).each(function(){
+      console.log($(this).text())
+      total = total+ Number($(this).text())
+      console.log('total is: ' + total)
+    })
+      $(this).text(total)
+      $(this).attr('class', 'finger-'+ total + ' finger ' + hand)
+}
+
+$start.on('click', function(){
+  theInterval = setInterval(countDown, 1000)
+})
+
+function countDown(){
+  $timer.text('Time Left: ' + count)
+  if (count <= 0){
+    clearInterval(theInterval)
+    $gameBoard.css('background-color', 'rgba(255, 255, 255, 0.59)')
+    $finger.off('click', increase)
+  }
+  count = count -1;
+}
