d36d5296ea42e8a44a5ceb95fde560e600424d67
Sara Gallagher <sara.p.gallagher@gmail.com>
2017-04-06 16:31:22 -0700

    made a clear winner

--- new.js
+++ new.js
@@ -8,40 +8,23 @@ var $oneLeft = $('#one-left')
 var $oneRight = $('#one-right')
 var $twoLeft = $('#two-left')
 var $twoRight = $('#two-right')
+var $scoreP1 = $('#score-p1')
+var $scoreP2 = $('#score-p2')
+var player1Score = 0
+var player2Score = 0
+var $topPlayer = $('.top-player')
+var $bottomPlayer = $('.bottom-player')
+var $winner = $('#winner')
+
+
 
 $finger.on('click', increase)
-
-function increase(){
-  var hand = $(this).attr("class").split(' ')[2]
-    console.log(hand);
-  var total = 0
-    $('.' + hand).each(function(){
-      console.log($(this).text())
-      total = total+ Number($(this).text())
-      console.log('total is: ' + total)
-    })
-      $(this).text(total)
-      $(this).attr('class', 'finger-'+ total + ' finger ' + hand)
-}
-
 $start.on('click', function(){
   theInterval = setInterval(countDown, 1000)
 })
 $reload.on('click', function(){
   document.location.reload(true)
 })
-
-function countDown(){
-  $timer.text('Time Left: ' + count)
-  if (count <= 0){
-    clearInterval(theInterval)
-    $gameBoard.css('background-color', 'rgba(255, 255, 255, 0.59)')
-    $finger.off('click', increase)
-  }
-  count = count -1;
-}
-
-
 $twoRight.on('mouseenter', function(){
   $oneRight.css({transform: ' rotateX(-180deg) rotateZ(16deg)'})
 })
@@ -66,3 +49,87 @@ $twoLeft.on('mouseenter', function(){
 $twoLeft.on('mouseleave', function(){
   $oneLeft.css({transform: 'rotate(180deg) rotateZ(0deg)'})
 })
+
+function countDown(){
+  $timer.text('Time Left: ' + count)
+  if (count <= 0){
+    clearInterval(theInterval)
+    $gameBoard.css('background-color', 'rgba(255, 255, 255, 0.59)')
+    $finger.off('click', increase)
+    whoWon()
+  }
+  count = count -1;
+}
+
+function increase(){
+  var hand = $(this).attr("class").split(' ')[2]
+    console.log(hand);
+  var total = 0
+    $('.' + hand).each(function(){
+      console.log($(this).text())
+      total = total+ Number($(this).text())
+      console.log('total is: ' + total)
+    })
+      $(this).text(total)
+      $(this).attr('class', 'finger-'+ total + ' finger ' + hand)
+
+}
+
+$finger.on('click', playerOneLScore)
+$finger.on('click', playerOneRScore)
+$finger.on('click', playerTwoRScore)
+$finger.on('click', playerTwoLScore)
+
+function playerOneLScore(){
+  console.log('checking score')
+  if($twoLeft[0].innerText == '5'){
+    player1Score = player1Score + 1
+
+    $scoreP1.text("Player One: " + player1Score)
+    $finger.off('click', playerOneLScore)
+  }
+}
+function playerOneRScore(){
+  console.log('checking score')
+  if($twoRight[0].innerText == '5'){
+    player1Score = player1Score + 1
+
+    $scoreP1.text("Player One: " + player1Score)
+    $finger.off('click', playerOneRScore)
+  }
+}
+function playerTwoRScore(){
+  console.log('checking score')
+  if($oneRight[0].innerText == '5'){
+    player2Score = player2Score + 1
+
+    $scoreP2.text("Player Two: " + player2Score)
+    $finger.off('click', playerTwoRScore)
+  }
+}
+function playerTwoLScore(){
+  console.log('checking score')
+  if($oneLeft[0].innerText == '5'){
+    player2Score = player2Score + 1
+    $scoreP2.text("Player Two: " + player2Score)
+    $finger.off('click', playerTwoLScore)
+  }
+}
+
+
+function whoWon(){
+  if(player2Score < player1Score){
+    console.log('PLAYA ONE')
+    $topPlayer.addClass('blue')
+    $winner.text('Player One Wins')
+  }
+  if(player2Score > player1Score){
+    console.log('PLAYA TWO')
+    $bottomPlayer.addClass('blue')
+    $winner.text('Player Two Wins!')
+  }
+  if(player2Score == player1Score){
+    console.log("its a tie");
+    $winner.text('We have a Tie! Start Over')
+  }
+}
